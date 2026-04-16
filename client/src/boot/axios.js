5d7bd3483bc87ed4eb98145f2cee2aae2e8b0405
Igor Petkovic <igr.petkovic@gmail.com>
2026-04-16 21:58:25 +0200

    implementiran jwt login, logout i osnovna zaštita ruta

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -5,6 +5,16 @@ const api = axios.create({
   baseURL: 'http://localhost:3000/api',
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default boot(({ app }) => {
   app.config.globalProperties.$api = api;
 });
--- server/src/index.js
+++ server/src/index.js
@@ -17,4 +17,7 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
 });
