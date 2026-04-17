b737b18f187f4c809f6dbeeb93758992389780c7
Igor Petkovic <igr.petkovic@gmail.com>
2026-04-17 23:17:27 +0200

    wizard povezan s referentnim podacima iz baze

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,9 @@ const cors = require('cors');
 require('dotenv').config();
 
 const testRoutes = require('./routes/testRoutes');
+const authRoutes = require('./routes/authRoutes');
 const requestRoutes = require('./routes/requestRoutes');
+const referenceRoutes = require('./routes/referenceRoutes');
 
 const app = express();
 
@@ -11,13 +13,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/test', testRoutes);
+app.use('/api/auth', authRoutes);
 app.use('/api/requests', requestRoutes);
+app.use('/api/reference', referenceRoutes);
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
-
-const authRoutes = require('./routes/authRoutes');
-app.use('/api/auth', authRoutes);
 });
