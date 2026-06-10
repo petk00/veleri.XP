f4521f3da6328b568de231da26cb477ce4320a73
Igor Petkovic <igr.petkovic@gmail.com>
2026-06-10 14:22:44 +0200

    fix: 401 interceptor, upload Ponude u NARUCENO statusu i info o formatima
    - Axios interceptor automatski odjavljuje korisnika pri 401 (istekla sesija)
    - Ponuda dozvoljena za upload u NARUCENO statusu (edge case: obrisana nakon odobrenja)
    - Dropdown u NARUCENO prikazuje Ponudu samo ako nedostaje, inače samo Otpremnica
    - Dodan hint s dozvoljenim formatima i limitom veličine ispod upload forme

--- client/src/boot/axios.js
+++ client/src/boot/axios.js
@@ -1,4 +1,5 @@
 import { boot } from 'quasar/wrappers';
+import { Notify } from 'quasar';
 import axios from 'axios';
 
 const api = axios.create({
@@ -7,16 +8,30 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
-
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-
   return config;
 });
 
-export default boot(({ app }) => {
+export default boot(({ app, router }) => {
   app.config.globalProperties.$api = api;
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+      if (error.response?.status === 401 && router.currentRoute.value.path !== '/login') {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        Notify.create({
+          type: 'negative',
+          message: 'Sesija je istekla. Molimo prijavite se ponovno.',
+        });
+        router.replace('/login');
+      }
+      return Promise.reject(error);
+    }
+  );
 });
 
 export { api };
